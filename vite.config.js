ec1201d72bb169ec85de388cc34852cb7e1f323b
Sijal Manandhar <sijal.m06@gmail.com>
2026-01-21 01:31:31 +0545

    update base url in vite.config, add CNAME

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-v2/',
-  plugins: [react()],
-  server: {
-    http: 'http://localhost',
-    port: 3000,
-  },
+	base: '/',
+	plugins: [react()],
+	server: {
+		http: 'http://localhost',
+		port: 3000,
+	},
 });
